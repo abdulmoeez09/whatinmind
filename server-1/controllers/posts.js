dc20b62b8ec1e196c91ef46d3e59a84acf0a421b
Abdulmoeez09 <moizmalik405@gmail.com>
2022-01-26 20:37:30 +0400

    updated folder

---
+++ server-1/controllers/posts.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+import PostMessage from '../models/postMessage.js'
+export const getPosts = async (req, res) => {
+  try {
+    const PostMessages = await PostMessage.find()
+    res.status(200).json(PostMessages)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
+export const createPost = async (req, res) => {
+  const post = req.body
+  const newPost = new PostMessage(post)
+  try {
+    newPost.save()
+    res.status(201).json(newPost)
+  } catch (error) {
+    res.status(409).json({ message: error.message })
+  }
+}
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params
+  const post = req.body
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send('No post with that id')
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  })
+  res.json(updatedPost)
+}
